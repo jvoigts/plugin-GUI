d22c85468e29209a41fae8e40180a8edbf30fd94
jvoigts <jvoigts@mit.edu>
2016-06-04 05:12:28 -0400

    added some debug prints

--- Source/Plugins/JuliaProcessor/image_test.jl
+++ Source/Plugins/JuliaProcessor/image_test.jl
@@ -3,6 +3,8 @@ function oe_process!(data,image)
 
 
 #@printf "image (%d x %d)\n"  size(image,1) size(image,2);
+
+println("data (",size(data,1)," x ",size(data,2),")") ;
 println("image (",size(image,1)," x ",size(image,2),")") ;
 
 x=size(image,1);
@@ -14,6 +16,9 @@ for i=1:size(image,1)
 
 	end;
 end;
+
+#image[1:size(data,1),1:size(data,2)]=data[1:size(data,1),1:size(data,2)];
+
 	#image=image+1;
 	image       = image/maximum(image);
 
